23c5828d38ab01b0fc798cf35d3661111db08b05
Ben <ben.a.osborn@gmail.com>
2025-04-29 15:07:24 +0100

    initial commit

---
+++ build/launch-Computer_Graphics_Coursework-RelWithDebInfo.cmd
@@ -0,0 +1,9 @@
+cd C:/Users/benao/Desktop/Code/Computer-Graphics-Coursework/source/
+set 
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"C:\Users\benao\Desktop\Code\Computer-Graphics-Coursework\build\RelWithDebInfo\Computer_Graphics_Coursework.exe"  
+pause
